7d41bb5d575607200f2d43f372d48a6a1f7288c2
Steve <slshotts77@gmail.com>
2021-03-26 12:11:22 -0400

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public int InventoryCount { get; set; }
+        public decimal Price { get; set; }
+        public bool IsFood { get; set; }
+    }
+
+    //public class ProductDbContext : DbContext
+    //{
+        //public DbSet<Product> Products { get; set; }
+        //public object Product { get; internal set; }
+    //}
+}
